29b229024745a302366332168a0271b2bbb04756
Your Name <you@example.com>
2017-12-23 21:01:29 +0800

    no message

--- IHealthCare/Module/Me/HCMeViewController.m
+++ IHealthCare/Module/Me/HCMeViewController.m
@@ -8,7 +8,10 @@
 
 #import "HCMeViewController.h"
 
-@interface HCMeViewController ()
+@interface HCMeViewController () <MMTableViewInfoDelegate>
+{
+    MMTableViewInfo *m_tableViewInfo;
+}
 
 @end
 
@@ -19,6 +22,41 @@
     
     self.title = @"个人中心";
     [self setBackBarButton];
+    
+    m_tableViewInfo = [[MMTableViewInfo alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    m_tableViewInfo.delegate = self;
+    
+    UITableView *contentTableView = [m_tableViewInfo getTableView];
+    contentTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+    contentTableView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:contentTableView];
+    
+    [self reloadMeView];
+}
+
+-(void)reloadMeView
+{
+    [m_tableViewInfo clearAllSection];
+    
+//    [self addProfileSection];
+//    [self addFrozenEmployeeSection];
+//    
+//    if ([self needAddressBookCell])
+//    {
+//        [self getAddressBookAuthor];
+//
+//        MFTableViewSectionInfo *addressSectionInfo = [MFTableViewSectionInfo sectionInfoDefault];
+//        MFTableViewCellInfo *cellInfo = [MFTableViewCellInfo cellForMakeSel:@selector(makeAddressBookCell:)
+//                                                                 makeTarget:self
+//                                                                  actionSel:@selector(synMemberInfo)
+//                                                               actionTarget:self
+//                                                                     height:90.0f
+//                                                                   userInfo:nil];
+//        [addressSectionInfo addCell:cellInfo];
+//        [m_tableViewInfo addSection:addressSectionInfo];
+//    }
+//
+//    [self addFunctionSection];
 }
 
 - (void)didReceiveMemoryWarning {
