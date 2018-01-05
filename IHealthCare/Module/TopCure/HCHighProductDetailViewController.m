4ffc353ae3cce255c0706acac3b466981d4a9c7a
Your Name <you@example.com>
2018-01-06 00:43:43 +0800

    no message

--- IHealthCare/Module/TopCure/HCHighProductDetailViewController.m
+++ IHealthCare/Module/TopCure/HCHighProductDetailViewController.m
@@ -334,7 +334,7 @@
     req.sign                = [dict objectForKey:@"paySign"];
     [WXApi sendReq:req];
     //日志输出
-    NSLog(@"appid=%@\npartid=%@\nprepayid=%@\nnoncestr=%@\ntimestamp=%ld\npackage=%@\nsign=%@",[dict objectForKey:@"appid"],req.partnerId,req.prepayId,req.nonceStr,(long)req.timeStamp,req.package,req.sign );
+    NSLog(@"appid=%@\npartid=%@\nprepayid=%@\nnoncestr=%@\ntimestamp=%ld\npackage=%@\nsign=%@",[dict objectForKey:@"appId"],req.partnerId,req.prepayId,req.nonceStr,(long)req.timeStamp,req.package,req.sign );
 }
 
 //appId = wxfddaeb6d71257dc9;
