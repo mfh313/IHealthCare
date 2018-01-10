f6f8834a41b8a79cfbd3612d4f14fc1efa0195a7
Your Name <you@example.com>
2018-01-10 23:24:26 +0800

    no message

---
+++ IHealthCare/HCComponent/HCCountStepView.m
@@ -0,0 +1,22 @@
+//
+//  HCCountStepView.m
+//  IHealthCare
+//
+//  Created by mafanghua on 2018/1/10.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "HCCountStepView.h"
+
+@interface HCCountStepView ()
+{
+
+    
+}
+
+@end
+
+@implementation HCCountStepView
+
+
+@end
