90fc13cf80a305e0bb35de96d559220deeb62140
mafanghua <980992720@qq.com>
2018-01-13 19:16:00 +0800

    no message

--- IHealthCare/Module/Order/HCOrderAddressCreateViewController.m
+++ IHealthCare/Module/Order/HCOrderAddressCreateViewController.m
@@ -257,7 +257,7 @@
     defaultSet.cellReuseIdentifier = @"defaultSet";
     defaultSet.attachKey = @"defaultSet";
     defaultSet.cellHeight = 70.0f;
-    [m_cellInfos addObject:defaultSet];
+//    [m_cellInfos addObject:defaultSet];
 }
 
 -(MFTableViewCellObject *)separatorCellObject
