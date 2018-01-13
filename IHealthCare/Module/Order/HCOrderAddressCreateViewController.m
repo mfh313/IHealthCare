f01a5d3114e74f46358d355ecc5b054de4f51b00
mafanghua <980992720@qq.com>
2018-01-13 15:50:24 +0800

    no message

--- IHealthCare/Module/Order/HCOrderAddressCreateViewController.m
+++ IHealthCare/Module/Order/HCOrderAddressCreateViewController.m
@@ -26,7 +26,105 @@
     self.title = @"新建收货地址";
     [self setBackBarButton];
     
+    m_tableView = [[MFUITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    m_tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    m_tableView.backgroundColor = [UIColor hx_colorWithHexString:@"F4F4F4"];
+    m_tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    m_tableView.dataSource = self;
+    m_tableView.delegate = self;
+    m_tableView.m_delegate = self;
+    [self.view addSubview:m_tableView];
+    
+    [self reloadTableView];
+    
     [self setBottomView];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return m_cellInfos.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    NSString *identifier = cellInfo.cellReuseIdentifier;
+    
+//    if ([identifier isEqualToString:@"productImage"])
+//    {
+//        return [self tableView:tableView productImageCellForIndexPath:indexPath];
+//    }
+//    else if ([identifier isEqualToString:@"productTitle"])
+//    {
+//        return [self tableView:tableView productTitleCellForIndexPath:indexPath];
+//    }
+//    else if ([identifier isEqualToString:@"productDetail"])
+//    {
+//        return [self tableView:tableView productDetailCellForIndexPath:indexPath];
+//    }
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.separatorInset = UIEdgeInsetsZero;
+    }
+    
+    cell.textLabel.text = identifier;
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    return cellInfo.cellHeight;
+}
+
+-(void)reloadTableView
+{
+    [self makeCellObjects];
+    
+    [m_tableView reloadData];
+}
+
+-(void)makeCellObjects
+{
+    [m_cellInfos removeAllObjects];
+    
+    MFTableViewCellObject *addressText = [MFTableViewCellObject new];
+    addressText.cellReuseIdentifier = @"addressText";
+    addressText.attachKey = @"name";
+    addressText.cellHeight = 70.0f;
+    [m_cellInfos addObject:addressText];
+    
+    MFTableViewCellObject *phone = [MFTableViewCellObject new];
+    phone.cellReuseIdentifier = @"addressText";
+    phone.attachKey = @"phone";
+    phone.cellHeight = 70.0f;
+    [m_cellInfos addObject:phone];
+    
+    MFTableViewCellObject *city = [MFTableViewCellObject new];
+    city.cellReuseIdentifier = @"citySelect";
+    city.attachKey = @"city";
+    city.cellHeight = 70.0f;
+    [m_cellInfos addObject:city];
+    
+    MFTableViewCellObject *addr = [MFTableViewCellObject new];
+    addr.cellReuseIdentifier = @"addressText";
+    addr.attachKey = @"addr";
+    addr.cellHeight = 70.0f;
+    [m_cellInfos addObject:addr];
+    
+    MFTableViewCellObject *defaultSet = [MFTableViewCellObject new];
+    defaultSet.cellReuseIdentifier = @"defaultSet";
+    defaultSet.attachKey = @"defaultSet";
+    defaultSet.cellHeight = 70.0f;
+    [m_cellInfos addObject:defaultSet];
 }
 
 -(void)setBottomView
