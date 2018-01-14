4a414f7b24ad45ec8bc74aab7c48db528bd2ffc9
mafanghua <980992720@qq.com>
2018-01-14 09:25:11 +0800

    no message

---
+++ IHealthCare/Module/HealthManagement/HCPlayerControlView.m
@@ -0,0 +1,15 @@
+//
+//  HCPlayerControlView.m
+//  IHealthCare
+//
+//  Created by mafanghua on 2018/1/14.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "HCPlayerControlView.h"
+
+@implementation HCPlayerControlView
+
+
+
+@end
