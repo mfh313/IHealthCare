77cd914c19430a8ec3385d6e700279d946150513
马方华 <980992720@qq.com>
2018-01-21 12:01:40 +0800

    no message

---
+++ IHealthCare/Module/Me/HCFavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HCFavoritesViewController.h
+//  IHealthCare
+//
+//  Created by 马方华 on 2018/1/21.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIViewController.h"
+
+@interface HCFavoritesViewController : MMUIViewController
+
+@end
