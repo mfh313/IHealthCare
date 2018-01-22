75069699e265bdc5273170339db081571b551892
mafanghua <980992720@qq.com>
2018-01-23 01:15:18 +0800

    no message

---
+++ IHealthCare/Module/Comments/HCAddCommentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HCAddCommentsViewController.h
+//  IHealthCare
+//
+//  Created by mafanghua on 2018/1/23.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIViewController.h"
+
+@interface HCAddCommentsViewController : MMUIViewController
+
+@end
---
+++ IHealthCare/Module/Comments/HCAddCommentsViewController.m
@@ -0,0 +1,26 @@
+//
+//  HCAddCommentsViewController.m
+//  IHealthCare
+//
+//  Created by mafanghua on 2018/1/23.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "HCAddCommentsViewController.h"
+
+@interface HCAddCommentsViewController ()
+
+@end
+
+@implementation HCAddCommentsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
