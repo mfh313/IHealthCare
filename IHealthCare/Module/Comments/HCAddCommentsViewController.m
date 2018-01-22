b94effaa6ff7e6e2254a19caac146884b9b34bab
mafanghua <980992720@qq.com>
2018-01-23 01:33:17 +0800

    no message

--- IHealthCare/Module/Comments/HCAddCommentsViewController.m
+++ IHealthCare/Module/Comments/HCAddCommentsViewController.m
@@ -34,6 +34,28 @@
         make.left.mas_equalTo(self.view.mas_left).offset(12);
         make.centerX.mas_equalTo(self.view.mas_centerX);
     }];
+    
+    UIButton *commentButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    commentButton.titleLabel.font = [UIFont systemFontOfSize:16.0f];
+    [commentButton setTitle:@"发表评论" forState:UIControlStateNormal];
+    [commentButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [commentButton setBackgroundImage:MFImageStretchCenter(@"common_btn_login_nor") forState:UIControlStateNormal];
+    [commentButton setBackgroundImage:MFImageStretchCenter(@"common_btn_login_press") forState:UIControlStateHighlighted];
+    [commentButton setBackgroundImage:MFImageStretchCenter(@"common_btn_login_dis") forState:UIControlStateDisabled];
+    [commentButton addTarget:self action:@selector(onClickCommentButton) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:commentButton];
+    
+    [commentButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(m_textView.mas_bottom).offset(27);
+        make.width.mas_equalTo(@(120));
+        make.height.mas_equalTo(@(40));
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+    }];
+}
+
+-(void)onClickCommentButton
+{
+    
 }
 
 - (void)didReceiveMemoryWarning {
