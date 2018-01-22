d82a6f1379dca9ed01651cc2a4a2b154c1e68a1b
mafanghua <980992720@qq.com>
2018-01-23 01:30:39 +0800

    no message

--- IHealthCare/Module/Comments/HCAddCommentsViewController.m
+++ IHealthCare/Module/Comments/HCAddCommentsViewController.m
@@ -8,7 +8,10 @@
 
 #import "HCAddCommentsViewController.h"
 
-@interface HCAddCommentsViewController ()
+@interface HCAddCommentsViewController () <UITextFieldDelegate>
+{
+    UITextView *m_textView;
+}
 
 @end
 
@@ -18,6 +21,19 @@
     [super viewDidLoad];
     
     self.title = @"发表评论";
+    [self setDismissBackButton];
+    
+    self.view.backgroundColor = [UIColor hx_colorWithHexString:@"EDEDED"];
+    
+    m_textView = [[UITextView alloc] init];
+    [self.view addSubview:m_textView];
+    
+    [m_textView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view.mas_top).offset(12);
+        make.height.mas_equalTo(@(120));
+        make.left.mas_equalTo(self.view.mas_left).offset(12);
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
